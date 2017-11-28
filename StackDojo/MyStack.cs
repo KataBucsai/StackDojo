9a4e8f9c194722dd139148dacf44f864edc1a173
KataBucsai <kata.kovacs@gmail.com>
2017-11-28 10:10:48 +0100

    implement MyStack

---
+++ StackDojo/MyStack.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackDojo
+{
+    class MyStack
+    {
+        public int Size { set; get; }
+        private string[] Stack;
+        public int Count { get; set; }
+
+
+        public MyStack(int size)
+        {
+            Stack = new string[size];
+            this.Size = size;
+            Count = 0;
+        }
+
+        public void Push(string newItem)
+        {
+            if (Count < Size)
+            {
+                Stack[Count] = newItem;
+                Count++;
+            } else
+            {
+                throw new StackOverflowException("Stackoverflow exception");
+            }
+        }
+
+        public void Pop()
+        {
+            if (Count > 1)
+            {
+                Count--;
+            } else
+            {
+                throw new InvalidOperationException("Stackunderflow exception");   
+            }
+        }
+
+        public string Peek()
+        {
+            return Stack[Count-1];
+        }
+
+        public int SpaceLeft()
+        {
+            return Size - Count;
+        }
+
+
+        public override string ToString()
+        {
+            string result = "";
+            for (int i = 0; i < Count; i++)
+            {
+                result += Stack[i] + " ";
+            }
+            return result;
+        }
+    }
+}
--- StackDojo/Program.cs
+++ StackDojo/Program.cs
@@ -10,6 +10,20 @@ namespace StackDojo
     {
         static void Main(string[] args)
         {
+            MyStack myStack = new MyStack(5);
+            myStack.Push("1");
+            myStack.Push("2");
+            myStack.Push("3");
+            Console.WriteLine(myStack.ToString());
+
+            myStack.Pop();
+            Console.WriteLine(myStack.ToString());
+
+            Console.WriteLine(myStack.Peek());
+            Console.WriteLine(myStack.Count);
+            Console.WriteLine(myStack.SpaceLeft());
+
+            Console.ReadLine();
         }
     }
 }
